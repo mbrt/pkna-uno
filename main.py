3497d863e7a892067544373383094cd979c64ea5
mbrt <m.bertasi@celonis.com>
2025-03-29 13:40:26 +0100

    Convert MD to JSON

--- main.py
+++ main.py
@@ -6,16 +6,33 @@ from google.genai import types
 from dotenv import load_dotenv
 import PIL.Image
 
+
 load_dotenv()
 
-root_dir = '../export/pkna-0'
+# Flags
+root_dir = '../export/pkna-03'
+
+
+def to_json(text: str) -> str:
+    out = []
+    for line in text.splitlines(keepends=True):
+        if line.startswith('```'):
+            continue
+        out.append(line)
+    return "".join(out)
+
+
+# Load the images
 image_paths = glob(os.path.join(root_dir, '*.jpg'))
 image_paths.sort()
-
 images = [
     PIL.Image.open(p)
     for p in image_paths
 ]
+
+print(f"Loaded {len(images)} images from {root_dir}")
+
+# Load prompts
 with open('prompt.md', 'r') as f:
     prompt = f.read().strip()
 with open('../export/characters.json', 'r') as f:
@@ -35,9 +52,12 @@ for i, image_chunk in enumerate(image_chunks):
         model="gemini-2.0-flash",
        contents=[prompt, characters] + image_chunk)  # type: ignore
 
-    out_file = os.path.join(root_dir, f'out-{i}.txt')
+    if response.text is None:
+        raise ValueError("Response text is None")
+    json_out = to_json(response.text)
+
+    out_file = os.path.join(root_dir, f'out-{i}.part.json')
     with open(out_file, 'a') as out:
-        out.write(response.text)  # type: ignore
-        out.write("\n")
+        out.write(json_out)
 
     print("Response written to file:", out_file)
