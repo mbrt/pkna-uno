b74419c43090229bd77af84699b81d5a6cc128d1
mbrt <michele.bertasi@gmail.com>
2025-08-07 11:39:11 +0200

    First version of new extract optimizer

--- dspy-extract-optimize.py
+++ dspy-extract-optimize.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
-from typing import Literal
+from typing import Literal, Any
+import json
 import os
 
 from dotenv import load_dotenv
 from pydantic import BaseModel, Field
 import dspy
+
+
+TRAINING_MODE = "light"
+EXAMPLES_PATH = "../output/dataset/reviewed.jsonl"
+OPTIMIZED_DIR = "../output/models"
 
 
 Character = Literal['uno', 'paperinik', 'other']
@@ -95,59 +101,94 @@ def init_llms() -> tuple[dspy.LM, dspy.LM]:
     return task_lm, prompt_lm
 
 
+def load_character(name: Character) -> CharacterDescription:
+    """Load a character description from the environment variable."""
+    p = os.path.join('../input/bios', f'{name}.md')
+    with open(p, 'r', encoding='utf-8') as f:
+        description = f.read()
+    return CharacterDescription(name=name, description=description)
+
+
 def validate(example: dspy.Example, pred: dspy.Prediction, trace=None) -> bool:
     """Validate the prediction."""
     return example.dialogue == pred.dialogue
 
 
-def optimize():
+def optimize(
+        examples: list[dspy.Example],
+        training_mode: Literal["light", "medium"],
+) -> Any:
     task_lm, prompt_lm = init_llms()
     characters = [
-        CharacterDescription(name='uno', description='A character named Uno.'),
+        load_character('uno'),
+        load_character('paperinik'),
         CharacterDescription(
-            name='paperinik', description='A character named Paperinik.'),
-        CharacterDescription(
-            name='other', description='Other characters in the comic book.'),
+            name='other',
+            description='Any other character in the comic book.'
+        ),
     ]
     extractor = ExtractorModule(characters=characters)
 
-    examples = [
-        x.with_inputs("page") for x in [
-            dspy.Example(
-                page=dspy.Image.from_file(
-                    '../input/pkna/pkna-0/pkna-0-029.jpg'),
-                dialogue=[
-                    ExtractedLine(character='uno', text='Hello!'),
-                    ExtractedLine(character='paperinik', text='Hi there!'),
-                ]
-            ),
-            dspy.Example(
-                page=dspy.Image.from_file(
-                    '../input/pkna/pkna-0/pkna-0-030.jpg'),
-                dialogue=[
-                    ExtractedLine(character='paperinik', text='Well well'),
-                ]
-            )
-        ]
-    ]
-
     teleprompter = dspy.MIPROv2(
         metric=validate,
-        auto="light",
+        auto=training_mode,
         prompt_model=prompt_lm,
         task_model=task_lm,
         num_threads=8,
         verbose=True,
     )
-    teleprompter.compile(
+    return teleprompter.compile(
         extractor,
         trainset=examples,
         requires_permission_to_run=False,
     )
 
 
+def classify_character(character: str) -> Character:
+    """Classify the character based on the text."""
+    match character.lower():
+        case 'uno':
+            return 'uno'
+        case 'pk' | 'paperinik':
+            return 'paperinik'
+        case _:
+            return 'other'
+
+
+def parse_reviewed(line: str) -> dspy.Example | None:
+    """Parse a line from the reviewed dataset."""
+    data = json.loads(line)
+    dialogue = data['ocr']['dialogue']
+
+    if not dialogue or len(dialogue) == 0:
+        return None
+
+    return dspy.Example(
+        page=dspy.Image.from_file(data['image']),
+        dialogue=[
+            ExtractedLine(
+                character=classify_character(line['character']),
+                text=line['text'],
+            )
+            for line in dialogue
+        ]
+    ).with_inputs('path')
+
+
+def build_dataset(examples_path: str) -> list[dspy.Example]:
+    res = []
+    with open(examples_path, 'r') as f:
+        for line in f:
+            example = parse_reviewed(line)
+            if example:
+                res.append(example)
+    return res
+
+
 def main():
-    optimize()
+    examples = build_dataset(EXAMPLES_PATH)
+    m = optimize(examples, TRAINING_MODE)
+    m.save(os.path.join(OPTIMIZED_DIR, 'dspy-extract-filtered.json'))
 
 
 if __name__ == "__main__":
