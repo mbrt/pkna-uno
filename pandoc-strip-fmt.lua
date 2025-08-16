d5cfd4ec2f43ee30c2f825e51404248841ace003
mbrt <michele.bertasi@gmail.com>
2025-08-16 22:01:38 +0200

    Extract from fandom

--- pandoc-strip-fmt.lua
+++ pandoc-strip-fmt.lua
@@ -1,11 +1,41 @@
+-- Strip links
 function Link(el)
     return el.content
 end
 
+-- Strip formatting
 function Strong(el)
   return el.content
 end
 
 function Emph(el)
   return el.content
+end
+
+-- Strip images
+function Image(el)
+    return {}
+end
+
+-- Strip footnotes
+function Note(el)
+  return {}
+end
+
+-- Strip tables
+function Table(el)
+  return {}
+end
+
+-- Strip HTML
+function RawInline(el)
+  if el.format:match("html") then
+    return {}
+  end
+end
+
+function RawBlock(el)
+  if el.format:match("html") then
+    return {}
+  end
 end
